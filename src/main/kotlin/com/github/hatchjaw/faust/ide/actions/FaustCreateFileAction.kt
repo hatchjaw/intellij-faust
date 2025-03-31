76f93b1441f8f0a896a21893a38b73fa7cc2a22f
Tommy Rushton <hatchjaw@users.noreply.github.com>
2025-03-31 16:21:11 +0200

    Better new-file approach.

--- src/main/kotlin/com/github/hatchjaw/faust/ide/actions/FaustCreateFileAction.kt
+++ src/main/kotlin/com/github/hatchjaw/faust/ide/actions/FaustCreateFileAction.kt
@@ -8,32 +8,14 @@ import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiDirectory
 
-class FaustCreateDSPFileAction : CreateFileFromTemplateAction(CAPTION, "What the goddamn hell?", FaustIcons.FaustDSP),
+class FaustCreateFileAction : CreateFileFromTemplateAction(FaustBundle.message("faust.file"), "", FaustIcons.FaustDSP),
     DumbAware {
 
-    private companion object {
-        private val CAPTION = FaustBundle.message("faust.dsp.file.type.description")
-    }
-
     override fun buildDialog(project: Project, dir: PsiDirectory, builder: CreateFileFromTemplateDialog.Builder) {
-        builder.setTitle(CAPTION)
-            .addKind(FaustBundle.message("faust.create.file"), FaustIcons.FaustDSP, "Faust DSP.dsp")
+        builder.setTitle(FaustBundle.message("faust.file.new"))
+            .addKind(FaustBundle.message("faust.dsp.file.type.display.name"), FaustIcons.FaustDSP, "Faust DSP.dsp")
+            .addKind(FaustBundle.message("faust.lib.file.type.display.name"), FaustIcons.FaustLib, "Faust Library.lib")
     }
 
-    override fun getActionName(dir: PsiDirectory?, newName: String, templateName: String?): String = CAPTION
-}
-
-class FaustCreateLibFileAction : CreateFileFromTemplateAction(CAPTION, "", FaustIcons.FaustLib),
-    DumbAware {
-
-    private companion object {
-        private val CAPTION = FaustBundle.message("faust.lib.file.type.description")
-    }
-
-    override fun buildDialog(project: Project, dir: PsiDirectory, builder: CreateFileFromTemplateDialog.Builder) {
-        builder.setTitle(CAPTION)
-            .addKind(FaustBundle.message("faust.create.file"), FaustIcons.FaustLib, "Faust Library.lib")
-    }
-
-    override fun getActionName(dir: PsiDirectory?, newName: String, templateName: String?): String = CAPTION
+    override fun getActionName(dir: PsiDirectory?, newName: String, templateName: String?): String = FaustBundle.message("faust.file")
 }
